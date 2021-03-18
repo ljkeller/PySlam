95902be8ac2e7299ae8878ef2d5738b39fdc74e0
ljkeller <kellerjlucas@gmail.com>
2021-03-17 23:59:44 -0500

    Minor touchups and comments

--- SLAM.py
+++ SLAM.py
@@ -25,15 +25,18 @@ def createArgumentParser():
     # required arguments
     parser.add_argument('image_path', type=str, help='Absolute or relative path of the video file')
 
-    # optional arguments
+    # optional arguments, require additional args
     parser.add_argument('-s', '--scale_percent', nargs='?', default=50, type=int, help='An integer percentage of preprocessor image scaling (default=50)')
     parser.add_argument('-m', '--feature_matches', nargs='?', default=10, type=int, help='An integer representing how many feature matches to visualiz (default 10)')
     parser.add_argument('-f', '--num_features', nargs='?', default=500, type=int, help='An integer representing the maximum number of features to retain (default 500)')
     parser.add_argument('-t', '--score_type', nargs='?', default=0, type=int, help='An integer representing the feature detection type of 0:Harris, or 1:Fast (default:0)')
+
+    # optional boolean action arguments, dont require additional args
     parser.add_argument('-d', '--debug', action='store_true', help='An argument that enables debug mode, which prints information & displays several screens')
 
     return parser
 
+# Simple wrapper to increase readability
 def getProgramArguments():
     return createArgumentParser().parse_args()
 
@@ -43,7 +46,7 @@ def main():
     args = getProgramArguments()
 
     # track recent data
-    # TODO: change feature to namedtuple('Feature', ['keypoint', 'descriptor'] & update code
+    # TODO: change features to tuple? named_tuple('Feature', ['keypoint', 'descriptor'] & update code
     frameDeque = deque(maxlen=2)
     kpDeque = deque(maxlen=2)
     desDeque = deque(maxlen=2)
@@ -96,7 +99,7 @@ def main():
                 # Homography model is good estimator for planar scenes
                 pose, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
 
-                # TODO: Get essential matrix here, then recover pose
+                # TODO: Get essential matrix here, then recover pose, find which model has min error
                 #reval, mask = cv2.findFundamentalMat(src_pts, dst_pts, cv2.FM_RANSAC)
 
                 # Make array in which x_i == 1 -> x_i feature is a match between src & dst points
