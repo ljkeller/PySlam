9d6ae677e006752f4e0e53de73918c6396571b51
ianmg <ianmsgames@gmail.com>
2021-03-20 14:25:31 -0500

    Files for streaming video from PI to different computer

---
+++ computer.py
@@ -0,0 +1,61 @@
+# server.py
+import io
+import socket
+import struct
+from PIL import Image
+import numpy as np
+import cv2
+import math
+
+def drawMethod(cv2Image):
+	#Method to draw a cv2 image to the screen
+	cv2.imshow('Window', cv2.cvtColor(cv2Image, cv2.COLOR_BGR2RGB))
+	cv2.waitKey(1)
+
+def pillowToNPArray(pillowImage):
+	return np.array(pillowImage)
+
+def stream():
+	# Adapted from https://jmlb.github.io/robotics/2017/11/22/picamera_streaming_video/
+	server_socket = socket.socket()
+	server_socket.bind(('0.0.0.0', 8000))
+	server_socket.listen(0)
+	# Accept a single connection and make a file-like object out of it
+	connection = server_socket.accept()[0].makefile('rb')
+	W, H = 28, 28
+
+	try:
+		while True:
+			# Read the length of the image as a 32-bit unsigned int.
+			image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+
+			if not image_len:
+				break
+			# Construct a stream to hold the image data and read the image
+			# data from the connection
+			image_stream = io.BytesIO()
+			image = None
+			try:
+				# reading jpeg image
+				image_stream.write(connection.read(image_len))
+				image = Image.open(image_stream)
+			except:
+				# if reading raw images: yuv or rgb
+				image = Image.frombytes('L', (W, H), image_stream.read())
+			# Rewind the stream
+			image_stream.seek(0)
+			
+			#Convert to cv2 usable image
+			newIm = pillowToNPArray(image)
+
+			drawMethod(newIm)	
+			
+	finally:
+		connection.close()
+		server_socket.close()
+		
+
+
+
+if __name__ == '__main__':
+	stream()
---
+++ pi.py
@@ -0,0 +1,46 @@
+# client.py
+import io
+import socket
+import struct
+import time
+import picamera
+
+# Adapted from https://jmlb.github.io/robotics/2017/11/22/picamera_streaming_video/
+
+def write_img_to_stream(stream):
+    connection.write(struct.pack('<L', stream.tell()))
+    connection.flush()
+    stream.seek(0)  #seek to location 0 of stream_img
+    connection.write(stream.read())  #write to file
+    stream.seek(0)
+    stream.truncate()
+
+
+def gen_seq():
+    stream = io.BytesIO()
+    while True:
+        yield stream
+        write_img_to_stream(stream)
+
+# Connect a client socket to server_ip:8000
+client_socket = socket.socket()
+ip_address = "192.168.1.20"
+client_socket.connect( ( ip_address, 8000 ) )
+# Make a file-like object out of the connection
+connection = client_socket.makefile('wb')
+
+if __name__ == '__main__':
+    try:
+        with picamera.PiCamera() as camera:
+            camera.resolution = (640,480)
+            #Adjust framerate to send higher number of pixels 
+            camera.framerate = 20
+            # Start a preview and let the camera warm up for 2 seconds
+            camera.start_preview()
+            time.sleep(2)
+            camera.stop_preview()
+            camera.capture_sequence(gen_seq(), "jpeg", use_video_port=True)
+        connection.write(struct.pack('<L', 0))
+    finally:
+        connection.close()
+        client_socket.close()
